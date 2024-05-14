5ddc9c5a29b4e5b8018498cb4997c259cccc9064
Edgar Fernandez <edgar1626fernandez@gmail.com>
2024-05-14 09:51:51 -0500

    training nair

--- efc_worker9.sh
+++ efc_worker9.sh
@@ -2,7 +2,7 @@
 #SBATCH --gpus-per-node=1
 #SBATCh --nodes=1
 #SBATCH --partition=thinkstation-p360
-#SBATCH --nodelist=worker8
+#SBATCH --nodelist=worker9
 #SBATCH --output="log_met.out"
 
 source /etc/profile.d/modules.sh
