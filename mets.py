6604a8530be1dbdb621a143ac6867be12c89f682
Edgar Fernandez <edgar1626fernandez@gmail.com>
2024-06-21 13:56:01 -0500

    metrics with wang

--- mets.py
+++ mets.py
@@ -103,7 +103,7 @@ def main():
         laterals = np.linspace(P.grid_xlims[0], P.grid_xlims[-1], num=128)
         grid = make_pixel_grid_from_pos(x_pos=laterals, z_pos=depths)
 
-        sub_row.append(n_sample)
+        sub_row.append(num_samples)
         sub_row.append(int(simu_name[4:]))
         r = P.radius
         cx = P.pos_lat
