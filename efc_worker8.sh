102cdc5d152c5989b95536930c7a12b205b39e90
Edgar Fernandez <edgar1626fernandez@gmail.com>
2024-06-25 11:10:06 -0500

    testing gan MODEL UDIFF

--- efc_worker8.sh
+++ efc_worker8.sh
@@ -3,8 +3,8 @@
 #SBATCh --nodes=1
 #SBATCH --partition=thinkstation-p360
 #SBATCH --nodelist=worker8
-#SBATCH --output="log_metrics_final.out"
+#SBATCH --output="log_genGAN.out"
 
 source /etc/profile.d/modules.sh
 module load jesus/1.0
-srun python /mnt/nfs/efernandez/projects/UNet_Nair/mets.py
+srun python /mnt/nfs/efernandez/projects/UNet_Nair/gen_WANG.py
