ad389feec467455b579efe1ead8ee06b88326738
Edgar Fernandez <edgar1626fernandez@gmail.com>
2024-06-26 12:06:54 -0500

    gen final test

--- efc_worker8.sh
+++ efc_worker8.sh
@@ -3,8 +3,8 @@
 #SBATCh --nodes=1
 #SBATCH --partition=thinkstation-p360
 #SBATCH --nodelist=worker8
-#SBATCH --output="log_genGAN.out"
+#SBATCH --output="log_gen_final_test.out"
 
 source /etc/profile.d/modules.sh
 module load jesus/1.0
-srun python /mnt/nfs/efernandez/projects/UNet_Nair/computting_metrics_diff_gan.py
+srun python /mnt/nfs/efernandez/projects/UNet_Nair/gen_samples_final_test.py
