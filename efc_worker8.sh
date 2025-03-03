07a153bdb1ce6bc8572a4155c84e279e70b66783
Edgar Fernandez <edgar1626fernandez@gmail.com>
2025-03-03 08:37:21 -0500

    adding worker7

--- efc_worker8.sh
+++ efc_worker8.sh
@@ -3,9 +3,8 @@
 #SBATCh --nodes=1
 #SBATCH --partition=thinkstation-p360
 #SBATCH --nodelist=worker8
-#SBATCH --output="log_gen_final_test.out"
+#SBATCH --output="log_worker8.out"
 
 source /etc/profile.d/modules.sh
 module load jesus/1.0
-srun python /mnt/nfs/efernandez/projects/UNet_Nair/computting_metrics_diff_gan.py
-.py
+srun python /mnt/nfs/efernandez/projects/UNet_Nair/SUST_new_dataset.py
